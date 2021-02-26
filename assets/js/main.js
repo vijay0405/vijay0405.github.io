db179c6f120a52bf0cabf9ee3636eb7538168e39
vijay0405 <uvijaysimha@gmail.com>
2021-02-26 12:10:52 +0530

    added scroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,31 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.home_title', {})
+sr.reveal('.button', {delay: 200})
+sr.reveal('.home_img', {delay: 400})
+sr.reveal('.home_social-icon', {interval: 200})
+
+sr.reveal('.about_img', {})
+sr.reveal('.about_subtitle', {delay: 200})
+sr.reveal('.about_text', {delay: 400})
+
+sr.reveal('.skills_subtitle', {})
+sr.reveal('.skills_text', {delay: 200})
+sr.reveal('.skills_data', {interval: 200})
+sr.reveal('.skills_img', {delay: 400})
+
+
+sr.reveal('.work_img', {interval: 200})
+
+
+sr.reveal('.contact_input', {interval: 200})
