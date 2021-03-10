c1881359fb34e3afc93c35d49d445c66d9a390de
vijay0405 <uvijaysimha@gmail.com>
2021-03-10 15:55:23 +0530

    updated active menu on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,26 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
 
 const sr = ScrollReveal({
     origin: 'top',
